27b832d086e1913932a91f0df51650b76c42de29
Elzmosis <bocajmore@gmail.com>
2025-12-07 01:19:12 +1300

    Explicitly set publicDir in vite config

--- vite.config.ts
+++ vite.config.ts
@@ -37,6 +37,7 @@ export default defineConfig({
   },
   base: process.env.NODE_ENV === "production" ? "/Gundrens-Gambit/" : "/",
   root: path.resolve(import.meta.dirname, "client"),
+  publicDir: path.resolve(import.meta.dirname, "client", "public"),
   build: {
     outDir: path.resolve(import.meta.dirname, "dist/public"),
     emptyOutDir: true,
